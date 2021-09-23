8e4c60905cfe84249c84b5d4272e75d918111cbb
claudio-code <suporte2424@outlook.com>
2021-09-23 15:31:56 -0300

    optimised import libs

--- src/main/java/com/learning/spring/library/api/model/entity/Book.java
+++ src/main/java/com/learning/spring/library/api/model/entity/Book.java
@@ -1,9 +1,6 @@
 package com.learning.spring.library.api.model.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
 @Builder
